7887267338668373e158f1f8f08ce6316ec25717
Sahil Dinanath <4vertigo2@gmail.com>
2022-06-02 22:29:46 +0200

    added beginnings of updateDetails function, changed userNameTxt -> usernameTxt.

--- restaurant_app/app/src/main/java/com/example/restaurant/SettingsActivity.java
+++ restaurant_app/app/src/main/java/com/example/restaurant/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.example.restaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SettingsActivity extends RegisterActivity{
 
@@ -10,16 +12,31 @@ public class SettingsActivity extends RegisterActivity{
         super.onCreate(savedInstanceState);
 
         activityInit();
+        TextView successText = new TextView(this);
+        registerContent.addView(successText);
+
         setContentView(registerContent);
         nameTxtField.setText(User.getUserFirstName());
         surnameTxtField.setText(User.getUserSurname());
-        userNameTxtField.setText(User.getUserLoginUsername());
+        usernameTxtField.setText(User.getUserLoginUsername());
         passwordTxtField.setText(User.getUserLoginPassword());
         phoneNumTxtField.setText(User.getUserPhoneNumber());
         registerBtn.setText("Save");
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                successText.setText("");
+                successText.setText("Updated Successfully");
+                updateDetails();
+            }
+        });
     }
 
-    private void addDetails(){
-        User user = new User();
+    private void updateDetails(){
+        User.setUserFirstName(nameTxtField.getText().toString());
+        User.setUserSurname(surnameTxtField.getText().toString());
+        User.setUserLoginUsername(usernameTxtField.getText().toString());
+        User.setUserLoginPassword(passwordTxtField.getText().toString());
+        User.setUserPhoneNumber(phoneNumTxtField.getText().toString());
     }
 }
