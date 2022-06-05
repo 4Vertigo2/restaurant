b48aabc80326c3ba3a8d4dd9e63a84987e052ce0
GyuoMi <darshnsingh10@gmail.com>
2022-06-05 12:55:40 +0200

    layout issue sorted

--- restaurant_app/app/src/main/java/com/example/restaurant/CustomerRequest.java
+++ restaurant_app/app/src/main/java/com/example/restaurant/CustomerRequest.java
@@ -16,7 +16,7 @@ public class CustomerRequest extends LinearLayout {
     Help helper = new Help();
     public CustomerRequest(Context c){
         super(c);
-        setOrientation(LinearLayout.VERTICAL);
+        setOrientation(LinearLayout.HORIZONTAL);
         id = new TextView(c);
         status = new TextView(c);
         restaurant = new TextView(c);
@@ -25,21 +25,25 @@ public class CustomerRequest extends LinearLayout {
         ToggleButton tglBtnThumbsDown;
 
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-//        lp.weight = 0;
-        addView(id);
+        lp.weight = 0;
+        id.setPadding(0,0,20,0);
+        addView(id,lp);
 
         LinearLayout right = new LinearLayout(c);
         right.setOrientation(LinearLayout.VERTICAL);
+        time.setPadding(0,0,0,10);
         right.addView(status);
         right.addView(restaurant);
         right.addView(time);
 
-        addView(right);
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        lp2.weight = 1;
+        addView(right, lp2);
     }
 
     public void populate(JSONObject jo){
         try {
-            id.setText(jo.getString("ORDER_ID"));
+            id.setText("#" + jo.getString("ORDER_ID"));
             status.setText(helper.convertCodeToStatus(jo.getInt("ORDER_STATUS")));
             restaurant.setText(jo.getString("RESTAURANT_NAME"));
             time.setText(jo.getString("ORDER_TIME"));
