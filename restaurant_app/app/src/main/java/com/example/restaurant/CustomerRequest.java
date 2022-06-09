0013d90f656e5481c91e1bf1c917165f16722425
GyuoMi <darshnsingh10@gmail.com>
2022-06-09 19:32:07 +0200

    added check to only allow rating once once order is rated, tglbuttons are not visible

--- restaurant_app/app/src/main/java/com/example/restaurant/CustomerRequest.java
+++ restaurant_app/app/src/main/java/com/example/restaurant/CustomerRequest.java
@@ -14,11 +14,14 @@ import org.json.JSONObject;
 public class CustomerRequest extends LinearLayout {
     TextView id ;
     int order_id;
+    double order_rating = 0;
     TextView status;
     TextView restaurant;
     TextView time;
     Help helper = new Help();
     PHPRequest php = new PHPRequest();
+    ToggleButton tglBtnThumbsUp;
+    ToggleButton tglBtnThumbsDown;
 
     public CustomerRequest(Context c, Activity act){
         super(c);
@@ -27,9 +30,9 @@ public class CustomerRequest extends LinearLayout {
         status = new TextView(c);
         restaurant = new TextView(c);
         time = new TextView(c);
-        ToggleButton tglBtnThumbsUp = new ToggleButton(c);
+        tglBtnThumbsUp = new ToggleButton(c);
         tglBtnThumbsUp.setText("↑");
-        ToggleButton tglBtnThumbsDown = new ToggleButton(c);
+        tglBtnThumbsDown = new ToggleButton(c);
         tglBtnThumbsDown.setText("↓");
         ContentValues cv = new ContentValues();
 
@@ -39,35 +42,32 @@ public class CustomerRequest extends LinearLayout {
         lp.weight = 0;
         id.setPadding(0,0,20,0);
         addView(id,lp);
+            tglBtnThumbsUp.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tglBtnThumbsDown.setChecked(false);
+                    tglBtnThumbsUp.setChecked(true);
+                    tglBtnThumbsUp.setText("↑");
+                    tglBtnThumbsDown.setText("↓");
+                    cv.put("rating", 1);
+                    cv.put("id", order_id);
+                    php.doRequest(act, "customer_rating", cv, null);
 
-        tglBtnThumbsUp.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tglBtnThumbsDown.setChecked(false);
-                tglBtnThumbsUp.setChecked(true);
-                tglBtnThumbsUp.setText("↑");
-                tglBtnThumbsDown.setText("↓");
-                cv.put("rating", 1);
-                cv.put("id", order_id);
-                php.doRequest(act, "customer_rating",cv ,null);
+                }
+            });
+            tglBtnThumbsDown.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tglBtnThumbsUp.setChecked(false);
+                    tglBtnThumbsDown.setChecked(true);
+                    tglBtnThumbsUp.setText("↑");
+                    tglBtnThumbsDown.setText("↓");
+                    cv.put("rating", 0);
+                    cv.put("id", order_id);
+                    php.doRequest(act, "customer_rating", cv, null);
+                }
+            });
 
-            }
-        });
-        tglBtnThumbsDown.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tglBtnThumbsUp.setChecked(false);
-                tglBtnThumbsDown.setChecked(true);
-                tglBtnThumbsUp.setText("↑");
-                tglBtnThumbsDown.setText("↓");
-                cv.put("rating", 0);
-                cv.put("id", order_id);
-                php.doRequest(act, "customer_rating",cv ,null);
-            }
-        });
-
-        addView(tglBtnThumbsUp);
-        addView(tglBtnThumbsDown);
 
         LinearLayout right = new LinearLayout(c);
         right.setOrientation(LinearLayout.VERTICAL);
@@ -79,19 +79,27 @@ public class CustomerRequest extends LinearLayout {
         LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         lp2.weight = 1;
         addView(right, lp2);
+
     }
 
     public void populate(JSONObject jo){
         try {
             id.setText("#" + jo.getString("ORDER_ID"));
+            order_rating = jo.getDouble("ORDER_RATING");
+            if(order_rating == 0.5){
+                addView(tglBtnThumbsUp);
+                addView(tglBtnThumbsDown);
+            }
             order_id = Integer.parseInt(jo.getString("ORDER_ID"));
             status.setText(helper.convertCodeToStatus(jo.getInt("ORDER_STATUS")));
             restaurant.setText(jo.getString("RESTAURANT_NAME"));
             time.setText(jo.getString("ORDER_TIME"));
+
         }
         catch(JSONException e){
             System.out.println("Order Class : JSON failed");
         }
 
     }
+
 }
