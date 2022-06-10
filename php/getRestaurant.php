c515c81504ebc55e827dd714bf97ae1706e9afcf
Kereshlin <105655937+Kereshlin@users.noreply.github.com>
2022-06-10 18:38:37 +0200

    Update and rename getRatingLevel.php to getRestaurant.php

--- php/getRestaurant.php
+++ php/getRestaurant.php
@@ -2,9 +2,9 @@
 $username="s2439564";
 $password="s2439564";
 $database="d2439564";
-$avg=$_REQUEST["avg_rating"];
+$resID=$_REQUEST["resID"];
 $link=mysqli_connect("127.0.0.1",$username,$password,$database);
-$sql_function="SELECT RatingLevel('$avg')";
+$sql_function="SELECT getRestaurant('$resID')";
 $r=mysqli_query($link,$sql_function);
 $row=$r->fetch_assoc();
 mysqli_close($link);
