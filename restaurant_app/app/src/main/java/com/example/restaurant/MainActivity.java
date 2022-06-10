3bbb543f70e9a7234ac525e28591aee7ccc917c1
Darshan Singh <88923579+GyuoMi@users.noreply.github.com>
2022-06-10 23:47:33 +0200

    Update MainActivity.java
    fixed splash screen
    removed status and navigation bar

--- restaurant_app/app/src/main/java/com/example/restaurant/MainActivity.java
+++ restaurant_app/app/src/main/java/com/example/restaurant/MainActivity.java
@@ -31,7 +31,18 @@ public class MainActivity extends AppCompatActivity {
         int imageResource = getResources().getIdentifier(uri, null, getPackageName());
 
         Drawable res = getResources().getDrawable(imageResource);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         imageView.setImageDrawable(res);
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
 
         /*from raw folder*/
